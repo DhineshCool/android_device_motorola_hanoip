b29e1fc7d1f0a325e56015bb96415a0425e60a8b
kytoaq <kytoaq@gmail.com>
2022-04-01 00:47:57 -0600

    hanoip: Allow LZ4 Compression
    * Hanoip can use LZ4 compression > GKI
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
 
+# LZ4 Compression
+BOARD_RAMDISK_USE_LZ4 := true
+
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
